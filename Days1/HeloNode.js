beadf5ac383d140a76be60ea5f32e7ab41743853
RamRaj110 <rampawankumar110@gmail.com>
2025-09-21 11:57:50 +0530

    Days1

--- Days1/HeloNode.js
+++ Days1/HeloNode.js
@@ -1,8 +1,12 @@
+
+
 let a = "Hello Node";
 console.log(a);
 
 // console.log(global);//global object in node js
-// console.log(this);//empty object in node js
+console.log(this);//empty object in node js
 
 console.log(globalThis);//global object in node js. globalThis is anywhere as global object in browser and node js.
+
+
 
